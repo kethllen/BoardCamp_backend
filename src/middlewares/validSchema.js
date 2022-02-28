a17a201487bb2df27f17bfcb496cd5f33792cc01
kethllen <kety_andrey@hotmail.com>
2022-02-28 14:20:30 -0300

    feat:POST of category validation

--- src/middlewares/validSchema.js
+++ src/middlewares/validSchema.js
@@ -1,12 +1,14 @@
-import joi from 'joi';
+import joi from "joi";
 
 export default function validSchema(schema) {
   return function (req, res, next) {
     const validation = schema.validate(req.body);
 
     if (validation.error) {
-      return res.status(422).send(validation.error.details.map(obj => (obj.message)));
+      return res
+        .status(422)
+        .send(validation.error.details.map((obj) => obj.message));
     }
     next();
-  }
+  };
 }
