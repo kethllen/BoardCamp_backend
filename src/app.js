7394287a5cc067ff25b798ee56c9bf707e79efcd
kethllen <kety_andrey@hotmail.com>
2022-02-28 13:28:27 -0300

    feat: Init project

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import router from "./routes/index.js";
+import dotenv from "dotenv";
+dotenv.config();
+//import connection from './database/database.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+app.listen(4000, () => {
+  console.log("Server is listening on port 4000.");
+});
