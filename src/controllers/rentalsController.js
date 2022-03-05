bcd0ac817e73f6ee521a61c4b04faaf7e23a3918
kethllen <kety_andrey@hotmail.com>
2022-03-05 12:08:27 -0300

    feat: get rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -2,8 +2,79 @@ import connection from "../database/database.js";
 
 export async function getRentals(req, res) {
   try {
-  } catch (error) {}
+    let gameId = "";
+    let customerId = "";
+    if (req.query.customerId) {
+      customerId = `WHERE rentals."customerId "= ${req.query.customerId}`;
+    }
+    if (req.query.gameId) {
+      gameId = `WHERE rentals."gameId"  = ${req.query.gameId}`;
+    }
+
+    const result = await db.query({
+      text: `
+          SELECT 
+            rentals.*,
+            customers.id,
+            customers.name,
+            games.id,
+            games.name,
+            games."categoryId",
+            categories.name
+          FROM rentals
+            JOIN customers ON rentals."customerId"=customers.id
+            JOIN games ON rentals."gameId"=games.id
+            JOIN categories ON games."categoryId"=categories.name
+          ${customerId}
+          ${gameId}
+      `,
+      rowMode: "array",
+    });
+
+    res.send(
+      result.rows.map((row) => {
+        const [
+          id,
+          customerId,
+          gameId,
+          rentDate,
+          daysRented,
+          returnDate,
+          originalPrice,
+          delayFee,
+          idCustomer,
+          nameCustomer,
+          idGame,
+          nameGame,
+          categoryId,
+          categoryName,
+        ] = row;
+
+        return {
+          id,
+          customerId,
+          gameId,
+          rentDate,
+          daysRented,
+          returnDate,
+          originalPrice,
+          delayFee,
+          customer: { id: idCustomer, name: nameCustomer },
+          game: {
+            id: idGame,
+            name: nameGame,
+            categoryId: categoryId,
+            categoryName: categoryName,
+          },
+        };
+      })
+    );
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 }
+
 export async function postRental(req, res) {
   try {
   } catch (error) {}
--- src/schemas/rentalSchema.js
+++ src/schemas/rentalSchema.js
@@ -1,14 +1,9 @@
 import joi from "joi";
 
 const rentalSchema = joi.object({
-  name: joi.string().required(),
   customerId: joi.number().required(),
   gameId: joi.number().required(),
-  rentDate: joi.date().required(),
   daysRented: joi.number().required(),
-  returnDate: joi.date(),
-  originalPrice: joi.number().required(),
-  delayFee: joi.number(),
 });
 
 export default rentalSchema;
